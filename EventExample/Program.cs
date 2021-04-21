c468947098ecb2974b4a3ee60483e42932850b83
Schmitzenbergh <35147851+Schmitzenbergh@users.noreply.github.com>
2021-04-21 10:26:43 +0200

    Add Rectangle.cs class. Expand example with events.

--- EventExample/Program.cs
+++ EventExample/Program.cs
@@ -6,7 +6,27 @@ namespace EventExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle r = new Rectangle();
+            r.Changed += new EventHandler(R_Changed); //Here we subscribe to the "Changed" event in the rectangle. Whenever the event is "fired", the R_Changed method will execute.
+            //r.Changed += R_Changed;
+
+            r.Length = 10; //We change the Length, thus the event should fire. 
+            r.Length = 20;
+            r.Length = 30;
+        }
+
+        private static void R_Changed(object sender, EventArgs e)
+        {
+            try
+            {
+                Rectangle r = (Rectangle)sender;
+                Console.WriteLine("Value changed: Length {0}", r.Length);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+            }
+            
         }
     }
 }
---
+++ EventExample/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventExample
+{
+    class Rectangle
+    {
+        public event EventHandler Changed; //Define an event called "Changed" other classes could subscribe to.
+
+        private double length;
+        public double Length
+        {
+            get { return length; }
+            set
+            {
+                length = value;
+                Changed(this, EventArgs.Empty);//Everytime the Length property changes the length field, the "Changed" event will fire thus informing subscribers.
+            }
+        }
+
+        public int id { get; private set; }
+    }
+}
