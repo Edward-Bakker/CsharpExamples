5455597b79d20521b175823cfa7cb1233774cf68
Schmitzenbergh <35147851+Schmitzenbergh@users.noreply.github.com>
2021-04-28 09:16:28 +0200

    Add new Databinding example

---
+++ DatabindingExample/MainWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DatabindingExample
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// This is the propertychanged event databindings listen to.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// The private movieList. This list can only be updated internally or via the designated property in MovieList.
+        /// </summary>
+        private List<Movie> movieList = new List<Movie>();
+
+        /// <summary>
+        /// A selected movie (updated via binding).
+        /// </summary>
+        private Movie selectedMovie;
+
+        /// <summary>
+        /// The Public MovieList property. This property has no real value as it uses the private movieList as its datastorage.
+        /// </summary>
+        public List<Movie> MovieList
+        {
+            get { return movieList; }
+            set
+            {
+                movieList = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// The Public SelectedMovie property. This property has no real value as it uses the private movieList as its datastorage.
+        /// </summary>
+        public Movie SelectedMovie
+        {
+            get { return selectedMovie; }
+            set
+            {
+                selectedMovie = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Constructor for MainWindowViewModel
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            movieList = MovieFactory.CreateRandomMoviesList();
+        }
+
+        /// <summary>
+        /// The important notifier method of changed properties. This function should be called whenever you want to inform other classes that some property has changed.
+        /// </summary>
+        /// <param name="propertyName">The name of the updated property. Leaving this blank will fill in the name of the calling property.</param>
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DatabindingExample/MovieFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabindingExample
+{
+    class MovieFactory
+    {
+        /// <summary>
+        /// Create a "random" list of movies (predefined) to test with.
+        /// </summary>
+        /// <returns>A list containing some movies.</returns>
+        public static List<Movie> CreateRandomMoviesList()
+        {
+            List<Movie> l = new List<Movie>();
+            l.Add(new Movie("Mad Max", "Action"));
+            l.Add(new Movie("Deadpool", "Comedy", new Uri("https://fanart.tv/fanart/movies/293660/movieposter/deadpool-56948e5115048.jpg"))); //Uri's might die over time, replace them if needed
+            l.Add(new Movie("Dracula", "Horror"));
+            l.Add(new Movie("The Matrix", "Sci-Fi", new Uri("https://c1.staticflickr.com/9/8819/17072638696_f871731849_b.jpg"))); //Uri's might die over time, replace them if needed
+            return l;
+        }
+    }
+}
