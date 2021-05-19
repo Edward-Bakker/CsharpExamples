1a3b783e78b69cefb4a221cf7694bb4135a80fcb
Schmitzenbergh <35147851+Schmitzenbergh@users.noreply.github.com>
2021-05-19 23:19:02 +0200

    Add ServiceExample to solution

---
+++ ServiceExample/ServiceExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceExample
+{
+    public partial class ServiceExample : ServiceBase
+    {
+        public ServiceExample()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            EventLog.WriteEntry("The NHL-Stenden service has started!", EventLogEntryType.Information);
+        }
+
+        protected override void OnStop()
+        {
+            EventLog.WriteEntry("The NHL-Stenden service has stopped!", EventLogEntryType.Information);
+        }
+    }
+}
